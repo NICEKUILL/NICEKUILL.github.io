47e5d3f40b2be3da92a71c3368bfc9b727e6fad6
bocilhengker <iknhiu7@gmail.com>
2022-06-15 08:06:38 +0700

    iixnicnnocwcb

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+<body>
+    <h1>hello world!<h/1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, atque unde maiores veritatis fugit sunt placeat ad enim distinctio illo sint fugiat quasi nobis perferendis iusto magnam explicabo vero harum?</p>
+    <ul>
+        <li>mantap</li>
+        <li>jempol</li>
+    </ul>
+</body>
+</html>
